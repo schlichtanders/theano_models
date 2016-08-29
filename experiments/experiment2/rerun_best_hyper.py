e3d1380f3352f3564360f7cfad8f5a6a28c74b06
Stephan Sahm <Stephan.Sahm@gmx.de>
2016-08-30 00:50:22 +0200

    subfolder rename

--- experiments/experiment2/rerun_best_hyper.py
+++ experiments/experiment2/rerun_best_hyper.py
@@ -55,10 +55,11 @@ elif len(sys.argv) > 2:
 elif len(sys.argv) > 1:
     foldername = sys.argv[1]
 
+subfolder = "final_hoffentlich"
 with ignored(OSError):
-    os.makedirs(os.path.join(__path__, "final", foldername))
+    os.makedirs(os.path.join(__path__, subfolder, foldername))
 
-filepath_tests = os.path.join(__path__, "final", foldername, "%s.db" % filename)
+filepath_tests = os.path.join(__path__, subfolder, foldername, "%s.db" % filename)
 # filepath_samples = os.path.join(__path__, foldername, "%s_samples.pkl" % datasetname)
 # -------------------------------------------
 
